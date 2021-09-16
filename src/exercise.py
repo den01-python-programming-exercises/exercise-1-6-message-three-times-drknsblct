ec8095672bbd0fbb6fc795a0501d0d663ef2c21a
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-16 03:53:48 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     #write your code below this line
     message = input("Write a message...")
+    for i in range(3):
+        print(message)
 
 if __name__ == '__main__':
     main()
